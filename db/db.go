5c869800a59d6bb1c1df3528fadbe7cb964cc0b5
yaaaaashiki <yaaaaakishi@gmail.com>
2017-08-18 11:39:07 +0900

    Add db connection

---
+++ db/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func gormConnect() *gorm.DB {
+	DBMS = "mysql"
+	USER = "root"
+	PASS = ""
+	//PROTOCOL = "tcp(##.###.##.###:3306)"
+	DBNAME = "user"
+
+	CONNECT = USER + ":" + PASS + "@" + "/" + erikachan
+	db, err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
