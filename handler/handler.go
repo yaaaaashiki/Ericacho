1648ea9091c42b62affbd27ac7470b1d61d6feb2
yaaaaashiki <yaaaaakishi@gmail.com>
2017-08-19 19:43:49 +0900

    Add output map

--- handler/handler.go
+++ handler/handler.go
@@ -8,8 +8,10 @@ import (
 )
 
 func RenderRoot(c echo.Context) error {
+	hogehoge := []string{"foo", "bar", "baz", "hogehoge", "kjfdjkj"}
+	huge := map[string]interface{}{
+		"foo": hogehoge,
+	}
 
-	return view.Slim(c, http.StatusOK, "something.slim", map[string]interface{}{
-		"foo": []string{"foo", "bar", "baz"},
-	})
+	return view.Slim(c, http.StatusOK, "something.slim", huge)
 }
