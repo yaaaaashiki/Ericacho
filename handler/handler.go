a269c78ded5d46604622f313d42e554f97bc0bc5
yaaaaashiki <yaaaaakishi@gmail.com>
2017-08-19 13:11:40 +0900

    Cannot rendering HTML

--- handler/handler.go
+++ handler/handler.go
@@ -4,10 +4,23 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/yaaaaashiki/Ericacho/view"
 )
 
 func RenderRoot() echo.HandlerFunc {
-	return func(c echo.Context) error {
-		return c.Render(http.StatusOK, "something.html", map[string]interface{}{})
+	return func(c echo.Context, w http.ResponseWriter, r *http.Request) error {
+		return view.Default(w, r, http.StatusOK, "something.slim", map[string]interface{}{})
 	}
 }
+
+/*
+func RenderRoot() error {
+	return func(c echo.Context, w http.ResponseWriter, r *http.Request) error {
+		return view.Default(w, r, http.StatusOK, "something.slim", map[string]interface{}{}),
+	}
+}
+*/
+/*
+func RenderRoot(c echo.Context) error {
+	return view.Slim(c, http.StatusOK, "test.slim", map[string]interface{}{})
+*/
--- server.go
+++ server.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"errors"
 	"html/template"
 	"io"
 	"log"
-	"os"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 	slim "github.com/mattn/go-slim"
 	"github.com/yaaaaashiki/Ericacho/controller"
 	"github.com/yaaaaashiki/Ericacho/handler"
-	"github.com/yaaaaashiki/Ericacho/view"
 )
 
 type Template struct {
@@ -29,21 +25,14 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-func Yashiki(who ...string) (string, error) {
-	err := errors.New("hogehoegkj")
-	return who[0], err
-}
-
 func main() {
 	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
 
 	renderer := &Template{
 		templates: template.Must(template.ParseGlob("templates/*.slim")),
 	}
 	e.Renderer = renderer
+
 	fs := make(map[string]slim.Func)
 
 	//	hoge := []string{"世界", "日本"}
@@ -54,16 +43,16 @@ func main() {
 		}
 	*/
 	//fmt.Println(hoge)
-
-	commits := map[string]interface{}{
-		"rsc": 3711,
-		"r":   2138,
-		"gri": 1908,
-		"adg": 912,
-	}
-	view.Init(fs)
-	view.Execute(os.Stdout, "/Users/Soichiro/go/src/github.com/yaaaaashiki/Ericacho/templates/something.slim", commits)
-
+	/*
+		commits := map[string]interface{}{
+			"rsc": 3711,
+			"r":   2138,
+			"gri": 1908,
+			"adg": 912,
+		}
+		view.Init(fs)
+		view.Execute(os.Stdout, "/Users/Soichiro/go/src/github.com/yaaaaashiki/Ericacho/templates/something.slim", commits)
+	*/
 	e.GET("/", handler.RenderRoot())
 
 	log.Println(controller.GetFirstUser())
